6dd307723c1c652526d486fc49ec9e85516a332c
Gustavo Batista <gustavo.ha.batista@gmail.com>
2023-10-06 15:57:17 -0300

    fix: lint issues

--- src/app/api/auth/[kindeAuth]/route.ts
+++ src/app/api/auth/[kindeAuth]/route.ts
@@ -1,6 +1,7 @@
 import { handleAuth } from '@kinde-oss/kinde-auth-nextjs/server'
 import { NextRequest } from 'next/server'
 
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
 export async function GET(request: NextRequest, { params }: any) {
   const endpoint = params.kindeAuth
   return handleAuth(request, endpoint)
